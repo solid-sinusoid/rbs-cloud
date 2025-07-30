9ccccbc5287c95ba59f6605fa3ab521e1e8f9bef
shalenikol <shaniks77s@gmail.com>
2025-07-30 15:21:31 +0300

    refact: rbs_cloud

--- rbs_cloud.py
+++ rbs_cloud.py
@@ -1,10 +1,13 @@
 import os
 import shutil
 from typing import List, Optional
+from enum import Enum
 from pathlib import Path
 
 import duckdb
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 from pydantic import BaseModel
 from fastapi import FastAPI, Query, HTTPException, File, UploadFile
 from fastapi.responses import FileResponse
@@ -15,29 +18,92 @@ app = FastAPI()
 DIR_DATA = "data"
 # Папка кэша
 DIR_CACHE = "cache"
-# Путь к parquet-файлу
+# Пути к parquet-файлам
 ID_FILE = DIR_DATA + "/file_ids.parquet"
+DATASET_FILE = DIR_DATA + "/datasets.parquet"
+WEIGHTS_FILE = DIR_DATA + "/weights.parquet"
+
+class DatasetStatus(str, Enum):
+    CREATING = "creating"
+    CONVERSION = "conversion"
+    STORE = "store"
+    AT_WORK = "at work" # is weights
 
 # Проверка наличия файла при старте
 if not os.path.exists(ID_FILE):
     # create id_file
     df = pd.DataFrame({
-        "name": ["RBS_ID"],
-        "cid": 0,
-        "sign": [""]
+        "name": ["RBS_ID", "datasets", "weights"],
+        "cid": [0, 0, 0],
+        "sign": ["", "", ""]
     })
     df.to_parquet(ID_FILE, index=False)
 
+if not os.path.exists(DATASET_FILE):
+    # Опишем схему
+    schema = pa.schema([
+        ("id", pa.int32()),
+        ("name", pa.string()),
+        ("num_episodes", pa.int32()),
+        ("src_format", pa.string()),
+        ("work_format", pa.string()),
+        ("status", pa.string())
+    ])
+    # Создаем пустую таблицу с заданной схемой
+    table = pa.Table.from_pandas(pd.DataFrame(columns=schema.names), schema=schema)
+    # Сохраняем таблицу в файл .parquet
+    pq.write_table(table, DATASET_FILE)
+
+if not os.path.exists(WEIGHTS_FILE):
+    schema = pa.schema([
+        ("id", pa.int32()),
+        ("name", pa.string()),
+        ("ref_up_ds", pa.int32()),
+        ("steps", pa.int32())
+    ])
+    table = pa.Table.from_pandas(pd.DataFrame(columns=schema.names), schema=schema)
+    pq.write_table(table, WEIGHTS_FILE)
 
 @app.get("/")
 def root():
-    return {"message": "DuckDB Parquet API работает!"}
+    return {"message": "Rbs Cloud (DuckDB Parquet API) работает!"}
 
+@app.post("/update-dataset/")
+async def update_dataset(new_record: dict, update_id: int):
+    # Открываем соединение с DuckDB
+    con = duckdb.connect()
+
+    try:
+        # Начинаем транзакцию
+        con.execute("BEGIN;")
+
+        # Добавляем новую запись в DATASET_FILE
+        new_df = pd.DataFrame([new_record])
+        new_df.to_parquet(DATASET_FILE, index=False, engine='pyarrow', append=True)
+
+        # Обновляем существующую запись в ID_FILE
+        con.execute(f"""
+            UPDATE '{ID_FILE}'
+            SET cid = cid + 1
+            WHERE name = 'RBS_ID';
+        """)
+
+        # Завершаем транзакцию
+        con.execute("COMMIT;")
+    except Exception as e:
+        # В случае ошибки откатываем транзакцию
+        con.execute("ROLLBACK;")
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        con.close()
+
+    return {"message": "Запись успешно добавлена и обновлена."}
 
 @app.get("/preview")
-def preview(limit: int = 10):
+def preview(file:str = "file_ids", limit: int = 10):
+    table = DIR_DATA + "/" + file + ".parquet"
     try:
-        df = duckdb.query(f"SELECT * FROM '{ID_FILE}' LIMIT {limit}").to_df()
+        df = duckdb.query(f"SELECT * FROM '{table}' LIMIT {limit}").to_df()
         return df.to_dict(orient="records")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
